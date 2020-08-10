188a2b0fd4a5887b2db4451ca1be1bc504e41c79
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2020-08-10 21:57:40 +0900

    i can run on device

--- ios/Pods.back/Target Support Files/flutter_isolate/flutter_isolate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_isolate : NSObject
-@end
-@implementation PodsDummy_flutter_isolate
-@end
--- ios/Pods.back/Target Support Files/flutter_user_agent/flutter_user_agent-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_flutter_user_agent : NSObject
-@end
-@implementation PodsDummy_flutter_user_agent
-@end
